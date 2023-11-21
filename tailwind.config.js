40c1bd960ea01be227a7d68a167cd6c7bd05e0c4
thallyslemos <thallysvilemos@gmail.com>
2023-11-21 11:09:39 -0300

    Add tw-elements package and update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--primary)',
-        secondary: 'var(--secondary)',
-        tertiary: 'var(--tertiary)',
-        quaternary: 'var(--quaternary)',
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        tertiary: "var(--tertiary)",
+        quaternary: "var(--quaternary)",
       },
       backgroundImage: {
-        'blue-gradient': 'var(--blue-gradient)',
+        "blue-gradient": "var(--blue-gradient)",
       },
       minHeight: {
-        'h-main': 'calc(100vh - 64px)',
-      }
+        "main-page": "calc(100vh - 64px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
 };
