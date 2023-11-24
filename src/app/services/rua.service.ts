f7957706ab4c88326825c253d88fdefe04087e1c
thallyslemos <thallysvilemos@gmail.com>
2023-11-23 23:20:25 -0300

    add methods

--- src/app/services/rua.service.ts
+++ src/app/services/rua.service.ts
@@ -4,15 +4,26 @@ import { Observable } from 'rxjs';
 import { Rua } from '../models/rua.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RuaService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private url = 'http://localhost:3000/ruas';
 
-  getRuas(): Observable<Rua[]> {
+  getAll(): Observable<Rua[]> {
     return this.http.get<Rua[]>(this.url);
+  }
+
+  create(rua: Rua): Observable<Rua> {
+    return this.http.post<Rua>(this.url, { nome: rua.nome });
+  }
+
+  update(rua: Rua): Observable<Rua> {
+    return this.http.put<Rua>(this.url + '/' + rua.id, { nome: rua.nome });
+  }
+
+  delete(id: number): Observable<Rua> {
+    return this.http.delete<Rua>(this.url + '/' + id);
   }
 }
