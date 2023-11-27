63d0a279da62e913f688901f2f49ef013274c7d3
thallyslemos <thallysvilemos@gmail.com>
2023-11-27 10:15:23 -0300

    Add warning message to protected routes

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard {
-  constructor(public authService: AuthService, public router: Router) {}
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    private toastr: ToastrService
+  ) {}
   canActivate():
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
     if (this.authService.isLoggedIn() !== true) {
-      window.alert('Acesso negado!');
+      this.toastr.warning('Faça login para acessar essa páina!');
       this.router.navigate(['login']);
     }
     return true;
